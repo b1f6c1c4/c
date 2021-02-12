176bad51dd64d5085d297c870c14673c71627fe4
b1f6c1c4 <b1f6c1c4@gmail.com>
2021-02-12 02:22:37 -0500

    read config from env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,14 @@
 const fs = require('fs');
 const JSON5 = require('json5');
 
-module.exports = JSON5.parse(fs.readFileSync('secret.json5', 'utf-8'));
+let cfg;
+try {
+  cfg = fs.readFileSync('secret.json5', 'utf-8');
+} catch {
+  cfg = process.env['C_FOR_COOK_CONFIG'];
+}
+
+if (!cfg)
+  throw new Error('Cannot found configuration from secret.json5 nor C_FOR_COOK_CONFIG');
+
+module.exports = JSON5.parse(cfg);
